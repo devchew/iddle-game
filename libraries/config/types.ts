3ea8f8a5325994380079912882e18234959c1e50
Devchew <the@devchew.com>
2025-06-22 19:03:11 +0200

    update typings

--- libraries/config/types.ts
+++ libraries/config/types.ts
@@ -1,45 +1,74 @@
-// Create TypeScript interfaces for our config data structures
+export type EntityType = 'building' | 'research' | 'fuel' ;
+
 export interface Entity {
+	// Unique identifier for the entity
 	id: string;
+	// Name of the entity
 	name: string;
-	tier: number;
-	type: string;
-	cost: Array<{ itemId: string; qty: number }>;
-	function: string;
-	operation_cost: string | number;
-	tech_requirement: string;
+
+	// type of the entity
+	type: EntityType;
+	// Time in seconds to craft this entity
+	craft_time: number; 
+	
+	// Amount of this entity produced per craft
+	craft_amount: number; 
+
+	// cost of creation
+	craft_cost: {
+		id: string; // ID of the entity
+		amount: number; // Amount of the entity required for this entity
+	}[];
+	// cost of operation
+	operation_cost: {
+		type: EntityType;
+		ammount: number; // Amount of the entity required for this operation
+	}[];
 }
 
 export interface Entities {
 	entities: Entity[];
 }
 
-export interface SciencePacks {
-	[key: string]: number;
-}
+
 
 export interface Upgrade {
 	id: string;
 	name: string;
-	requirement?: string;
-	science_packs: SciencePacks;
-	effect: string;
+	effects: {
+		id: string; // ID of the entity or research this upgrade affects
+		element: string; // The element to modify (e.g., 'craft_time', 'craft_amount')
+		amount: number; // The amount to modify the element by
+	}
 }
 
-export interface Unlock {
-	id: string;
-	name: string;
-	requirement: string;
-	science_packs: SciencePacks;
-	function: string;
-}
-
-export interface Tier {
-	tier: number;
+export interface Upgrades {
 	upgrades: Upgrade[];
-	unlocks: Unlock[];
+}
+
+export interface Research {
+	// Unique identifier for the research
+	id: string;
+	// Name of the research
+	name: string;
+	/**
+	 * The requirement for this research to be completed
+	 */
+	requirement: {
+		id: string; // ID of the entity or research this research depends on
+		ammount: number; // Optional amount for entities
+	}[];
+	// ids of entities that this research unlocks
+	unlocks?: {
+		id: string; // ID of the entity to unlock
+	}[];
+	// ids of upgrades that this research unlocks
+	upgrade?: {
+		id: string; // ID of the upgrade to apply
+	}
+	
 }
 
 export interface ResearchTree {
-	tiers: Tier[];
+	tiers: Research[];
 }
